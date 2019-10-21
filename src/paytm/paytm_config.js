3219d06d854fa917b8040849d57fa14af728b0aa
ank-21 <ankitSrivastava21598@gmail.com>
2019-10-21 23:10:40 +0530

    content change and paytm config details changed

--- src/paytm/paytm_config.js
+++ src/paytm/paytm_config.js
@@ -1,10 +1,10 @@
 module.exports = {
   paytm_config: {
-		MID: 'Ueaoue06440985709040',
+		MID: 'IQtjUZ20699413265628',
 		WEBSITE: 'DEFAULT',
     CHANNEL_ID: 'WEB',
     INDUSTRY_TYPE_ID: 'Retail',
-    MERCHANT_KEY : 'pSWlEuGTF#@n9pZU'
+    MERCHANT_KEY : 'vvKuVGkn@!GaI7!0'
   }
   // paytm_config: {
 	// 	MID: 'DVZAVM84453095483388',
